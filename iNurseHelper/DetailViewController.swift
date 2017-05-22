cd3c8a26855f31df7fc76aa5652c94d2b3e86fe3
Janice Richards <janlep47@gmail.com>
2017-05-22 14:01:37 -0400

    Upgrade source files for newer version of Swift (3.1)

--- iNurseHelper/DetailViewController.swift
+++ iNurseHelper/DetailViewController.swift
@@ -13,31 +13,31 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail: AnyObject = self.detailItem {
-            if let label = self.detailDescriptionLabel {
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
                 label.text = detail.description
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+        configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
     }
 
 
